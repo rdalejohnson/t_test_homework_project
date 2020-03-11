51bea70ce8414b437891e53f65dce79b7164151c
Robert Johnson <robertjohnson@Roberts-Mini.attlocal.net>
2020-03-11 17:57:45 -0500

    Adding more to homework solution

--- Homework_3_Two_Hypotheses.R
+++ Homework_3_Two_Hypotheses.R
@@ -1,4 +1,5 @@
 library(plyr)
+library(tidyverse)
 
 #Two Questions
 #1 
@@ -15,16 +16,47 @@ library(plyr)
 
 nylsData =read.csv("NYLS97_Substance.csv",sep=",") 
 
-count(nylsData, 'Race')
+plyr::count(nylsData, 'Race')
 
-count(nylsData, 'Substance1997')
+plyr::count(nylsData, 'Substance1997')
 
-count(nylsData, 'Substance2000')
+plyr::count(nylsData, 'Substance2000')
 
 nylsData$Substance2000[nylsData$Substance2000 < 0] <- NA
 
-count(nylsData, 'Race')
+plyr::count(nylsData, 'Race')
 
-count(nylsData, 'Substance1997')
+plyr::count(nylsData, 'Substance1997')
 
-count(nylsData, 'Substance2000')
+plyr::count(nylsData, 'Substance2000')
+
+
+nylsData <- nylsData %>% 
+  mutate(SubAbuseDelta = (Substance2000-Substance1997))
+
+plyr::count(nylsData, 'SubAbuseDelta')
+
+
+hitsgram=hist(nylsData$SubAbuseDelta  , main="a histogram of the difference",) 
+
+summary(nylsData$SubAbuseDelta)
+
+subAbuseStDev = sd(nylsData$SubAbuseDelta  ,na.rm=TRUE)
+subAbuseMean = mean(nylsData$SubAbuseDelta  ,na.rm=TRUE)
+
+ks.test(x= nylsData$SubAbuseDelta,"pnorm",mean=subAbuseMean,sd=subAbuseStDev)
+
+qqnorm(nylsData$SubAbuseDelta)
+qqline(nylsData$SubAbuseDelta)
+
+drinking_problem_score_plot <-
+  boxplot(nylsData$SubAbuseDelta,
+          main = "Drinking Problem Scores (10 item questionnaire)",
+          xlab = "Summarized Scores for 1120 Students",
+          col = "orange",
+          border = "brown",
+          horizontal = TRUE,
+          notch = TRUE)
+
+drinking_problem_score_plot
+
