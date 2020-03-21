9672517af7b11cc3f2216599f8c1098dc3cd1c27
Robert Johnson <robertjohnson@Roberts-Mini.attlocal.net>
2020-03-21 11:05:19 -0500

    Much better statistics, breakdowns by race and year added.

--- Homework_3_Two_Hypotheses.R
+++ Homework_3_Two_Hypotheses.R
@@ -6,6 +6,7 @@ library(tidyverse)
 # https://www.statsandr.com/blog/do-my-data-follow-a-normal-distribution-a-note-on-the-most-widely-used-distribution-and-how-to-test-for-normality-in-r/
 # https://statistics.berkeley.edu/computing/r-t-tests
 # http://www.sthda.com/english/articles/24-ggpubr-publication-ready-plots/76-add-p-values-and-significance-levels-to-ggplots/
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3693611/
 
 
 #Two Questions
@@ -37,6 +38,54 @@ plyr::count(nylsData, 'Substance1997')
 
 plyr::count(nylsData, 'Substance2000')
 
+
+# Descriptive Stats for entire year 1997 
+subAbuse1997Mean = mean(nylsData$Substance1997  ,na.rm=TRUE)
+subAbuse1997StDev = sd(nylsData$Substance1997  ,na.rm=TRUE)
+print (paste("1997 NLSY97 Substance Use Index Score Mean: ", subAbuse1997Mean))
+print (paste("1997 NLSY97 Substance Use Index Score St. Dev: ", subAbuse1997StDev))
+summary(nylsData$Substance1997)
+
+# Descriptive Stats for entire year 2000 
+subAbuse2000Mean = mean(nylsData$Substance2000  ,na.rm=TRUE)
+subAbuse2000StDev = sd(nylsData$Substance2000  ,na.rm=TRUE)
+print (paste("2000 NLSY97 Substance Use Index Score Mean: ", subAbuse2000Mean))
+print (paste("2000 NLSY97 Substance Use Index Score St. Dev: ", subAbuse2000StDev))
+summary(nylsData$Substance2000)
+
+
+############# NON-WHITES SUBSET of Data Set
+nonWhitesSubset = subset(nylsData, Race=="0")
+
+subAbuse1997NonWhiteMean = mean(nonWhitesSubset$Substance1997  ,na.rm=TRUE)
+subAbuse1997NonWhiteStDev = sd(nonWhitesSubset$Substance1997 ,na.rm=TRUE)
+print (paste("1997 NLSY97 Substance Use Index Score Mean, Non-Whites only: ", subAbuse1997NonWhiteMean))
+print (paste("1997 NLSY97 Substance Use Index Score St. Dev, Non-Whites only: ", subAbuse1997NonWhiteStDev))
+summary(nonWhitesSubset$Substance1997)
+
+
+subAbuse2000NonWhiteMean = mean(nonWhitesSubset$Substance2000  ,na.rm=TRUE)
+subAbuse2000NonWhiteStDev = sd(nonWhitesSubset$Substance2000 ,na.rm=TRUE)
+print (paste("2000 NLSY97 Substance Use Index Score Mean, Non-Whites only: ", subAbuse2000NonWhiteMean))
+print (paste("2000 NLSY97 Substance Use Index Score St. Dev, Non-Whites only: ", subAbuse2000NonWhiteStDev))
+summary(nonWhitesSubset$Substance2000)
+
+
+############# WHITES SUBSET of Data Set
+WhitesSubset = subset(nylsData, Race=="1")
+
+subAbuse1997WhiteMean = mean(WhitesSubset$Substance1997  ,na.rm=TRUE)
+subAbuse1997WhiteStDev = sd(WhitesSubset$Substance1997 ,na.rm=TRUE)
+print (paste("1997 NLSY97 Substance Use Index Score Mean, Non-Whites only: ", subAbuse1997WhiteMean))
+print (paste("1997 NLSY97 Substance Use Index Score St. Dev, Non-Whites only: ", subAbuse1997WhiteStDev))
+summary(WhitesSubset$Substance1997)
+
+
+subAbuse2000WhiteMean = mean(WhitesSubset$Substance2000  ,na.rm=TRUE)
+subAbuse2000WhiteStDev = sd(WhitesSubset$Substance2000 ,na.rm=TRUE)
+print (paste("2000 NLSY97 Substance Use Index Score Mean, Non-Whites only: ", subAbuse2000WhiteMean))
+print (paste("2000 NLSY97 Substance Use Index Score St. Dev, Non-Whites only: ", subAbuse2000WhiteStDev))
+summary(WhitesSubset$Substance2000)
 
 nylsData <- nylsData %>% 
   mutate(SubAbuseDelta = (Substance2000-Substance1997))
